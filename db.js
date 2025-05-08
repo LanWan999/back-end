01588d80e01aba2c28bc0494bb17dcc89dc2ded5
LanWan999 <name@example.com>
2025-05-08 13:27:53 +0300

    spelling mistake

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const process = require('process')
 
-mongoose.connect(process.env.DB_URi)
+mongoose.connect(process.env.DB_URI)
     .then(() => {
         console.log('MongoDB Connected')
     })
